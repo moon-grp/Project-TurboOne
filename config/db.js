ad57b02df51f41f45e5e56eeb7b886432cafcded
emohex <olumidemm@gmail.com>
2020-10-01 16:27:40 +0100

    url configurations

--- config/db.js
+++ config/db.js
@@ -6,6 +6,9 @@ const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
     });
     console.log("MongoDb connected....");
   } catch (err) {
--- routes/url.js
+++ routes/url.js
@@ -1,5 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const validUrl = require("valid-url");
+const shortId = require("shortid");
+const config = require("config");
 
+const Url = require("../models/url");
 
-module.exports = router
+router.post("shorten", async (res, req) => {
+  const { longUrl } = req.body;
+  const baseUrl = config.get("baseUrl");
+
+  if (!validUrl.isUri(baseUrl)) {
+    return res.status(401).json("Invalid base Url");
+  }
+
+  //generate code
+  const urlCode = shortId.generate();
+
+  //check long url code
+  if (validUrl.isUri(longUrl)) {
+    try {
+      let url = await Url.findOne({ longUrl });
+      if (url) {
+        res.json(url);
+      } else {
+        const shortUrl = baseUrl + "/" + urlCode;
+      }
+    } catch (err) {}
+  }
+});
+
+module.exports = router;
